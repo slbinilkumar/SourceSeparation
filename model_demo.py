6ae540e803b35c3c46341e44292c6004aed82b40
Corentin Jemine <corentin.jemine@gmail.com>
2019-04-16 20:25:33 +0200

    The dataset now subclasses tf.data.Dataset

--- model_demo.py
+++ model_demo.py
@@ -8,29 +8,37 @@ from pathlib import Path
 dataset_root = Path(r"E:\Datasets\Midi")
 
 batch_size = 32
-sample_duration = 5
+chunk_duration = 5
 
 if __name__ == "__main__":
     instruments = [get_instrument_id("Trumpet")]
     dataset = MidiDataset(
-        dataset_root, 
+        root=dataset_root, 
         is_train=True, 
-        sample_duration=5,
+        chunk_duration=chunk_duration,
         source_instruments=instruments, 
         target_instruments=instruments,
+        batch_size=batch_size,
+        # Fixme: don't forget to put a larger buffer size when testing things, we put a small one 
+        #  for quicker debugging only.
+        shuffle_buffer= batch_size,
     )
-    # Fixme: don't forget to put a larger buffer size when testing things, we put a small one for
-    #   quicker debugging only.
-    dataset = dataset.generate_dataset(batch_size, shuffle_buffer=batch_size)   
+    
     # Yields a single batch. Both x_train and y_train are of shape (batch_size, n_samples)
     x_train, y_train = next(iter(dataset))  
+    
+    # If you want to have a look at the data
+    import sounddevice as sd
+    for i in range(5):
+        print("Playing chunk %d" % i)
+        sd.play(x_train[i], 44100, blocking=True)
 
     # Example model
     model = keras.Sequential([
-        kl.InputLayer(input_shape=(sample_duration * sample_rate,)),
+        kl.InputLayer(input_shape=(chunk_duration * sample_rate,)),
         kl.Dense(1024, activation='relu'),
         kl.Dense(1024, activation='relu'),
-        kl.Dense(sample_duration * sample_rate, activation='tanh')
+        kl.Dense(chunk_duration * sample_rate, activation='tanh')
     ])
 
     model.compile(
