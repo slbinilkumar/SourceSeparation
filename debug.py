9a0e0a60f851c21b3ce56ecb38ec1a51082b8cef
Corentin Jemine <corentin.jemine@gmail.com>
2019-04-27 01:47:10 +0200

    Updated spectrogram computation to torch

--- debug.py
+++ debug.py
@@ -4,24 +4,23 @@ import librosa.display
 import librosa
 import torch
 
+
 fpath =  r"D:\Users\Corentin\Music\Pure White Poison.mp3"
 wav, sr = librosa.load(fpath, 44100)
 wav = wav[:100000]
 wav_t = torch.tensor(wav)
 
+def spectrogram(wav, win_size, hop_size, top_db=80.0):
+    stft = torch.stft(wav, win_size, hop_size, win_size, window=torch.hann_window(win_size))
+    power = (stft ** 2).sum(dim=2)
+    log_spec = 10. * torch.log10(power / power.max())
+    torch.max(log_spec, log_spec.max() - top_db, out=log_spec)
+    return log_spec
 
-def spectrogram(wav, win_size, hop_size, amin=1e-10, top_db=80.0):
-    stft = tf.transpose(tf.signal.stft(wav, win_size, hop_size, win_size), (1, 0))
-    power = tf.square(tf.abs(stft))
-    log_spec = tf.math.log(tf.maximum(amin, power / tf.reduce_max(power)))
-    log_spec = 10.0 * log_spec / tf.math.log(tf.constant(10.))
-    return tf.maximum(log_spec, tf.reduce_max(log_spec) - top_db)
-
-
-d = spectrogram(wav, sr // 20, sr // 80)
+d = spectrogram(wav_t, sr // 20, sr // 80).numpy()
 
 plt.figure(figsize=(12, 8))
-librosa.display.specshow(d.numpy(), y_axis='linear')
+librosa.display.specshow(d, y_axis='linear')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Linear-frequency power spectrogram')
 plt.show()
